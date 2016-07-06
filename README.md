6a778e9d74d3438c67afa8045a0a00bb65900691
Simon Ho <simonho@ca.ibm.com>
2016-07-06 01:30:16 -0700

    Add lat-owners script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# loopback-admin
+# loopback-admin-tools
+
 Administration scripts and tools for LoopBack related projects.
+
+## Usage
+
+> Until we publish this module to NPM, you can clone this repo locally and run
+`npm link` instead of `npm install -g loopback-admin-tools` to use this module.
+
+1. Install this module globally (`npm install -g loopback-admin-tools`) to get
+   a set of LoopBack administration related commands.
+1. Run a command (ie. `lat-owners` will generate `owners.md` in the current
+   dir).
+
+### `lat-owners`
+
+Generate `owners.md` in the **current working directory** using the list from [`loopback-owners`](//github.com/strongloop/loopback-owners/blob/master/index.js).
+
+> `owners.md` is a list of repository owners based on functional areas (ie.
+> Connectors, Juggler, Remoting, etc).
---
+++ bin/lat-owners.js
@@ -0,0 +1,129 @@
+#!/usr/bin/env node
+
+var f = require('util').format;
+var fs = require('fs');
+var groups = require('loopback-owners');
+var path = require('path');
+
+var filePath = path.resolve(process.cwd() + '/owners.md');
+
+fs.access(filePath, fs.F_OK, function(err) {
+  if (!err)
+    fs.truncateSync(filePath, 0);
+
+  groups.forEach(function(group, index) {
+    // print title
+    var title = '';
+    if (index !== 0)
+      title += '\n\n'
+    title += '# ' + group.name;
+    fs.appendFileSync(filePath, title);
+
+    if (group.subgroups) {
+      group.subgroups.forEach(function(subgroup) {
+        // print subgroup title
+        var subgroupTitle = '';
+        subgroupTitle += '\n\n## ' + subgroup.name;
+        fs.appendFileSync(filePath, subgroupTitle);
+
+        // print table header
+        var tableHeader = '\n\n<table>\n  <thead>\n    <tr>\n      <th width="100%" ' +
+          'align="left">Repo\n      <th>Owner\n      <th>Backup';
+        fs.appendFileSync(filePath, tableHeader);
+
+        // print table body
+        var tableBody = '\n  <tbody>';
+        fs.appendFileSync(filePath, tableBody);
+
+        // print subgroup rows
+        if (typeof subgroup.repos === 'string')
+          subgroup.repos = [subgroup.repos];
+        subgroup.repos && subgroup.repos.forEach(function(repo, index) {
+          var row = '\n    <tr>'
+
+          // add repo link
+          row += f('<td><a href="//github.com/%s">%s</a>',
+            repo, repo.split('/').pop());
+
+          if (index === 0) {
+            // add owner
+            row += f('\n        <td rowspan="%s" align="center">',
+              subgroup.repos.length);
+            if (typeof subgroup.owner === 'string')
+              subgroup.owner = [subgroup.owner];
+            subgroup.owner && subgroup.owner.forEach(function(owner, index) {
+              if (index !== 0)
+                row += '<br>'
+              row += owner;
+            });
+
+            // add backup
+            row += f('\n        <td rowspan="%s" align="center">',
+              subgroup.repos.length);
+            if (typeof subgroup.backup === 'string')
+              subgroup.backup = [subgroup.backup];
+            subgroup.backup && subgroup.backup.forEach(function(backup, index) {
+              if (index !== 0)
+                row += '<br>'
+              row += backup;
+            });
+          }
+
+          fs.appendFileSync(filePath, row);
+        });
+
+        // print table footer
+        fs.appendFileSync(filePath, '\n</table>');
+      });
+    } else {
+      // print table header
+      var tableHeader = '\n\n<table>\n  <thead>\n    <tr>\n      <th width="100%" ' +
+        'align="left">Repo\n      <th>Owner\n      <th>Backup';
+      fs.appendFileSync(filePath, tableHeader);
+
+      // print table body
+      var tableBody = '\n  <tbody>';
+      fs.appendFileSync(filePath, tableBody);
+
+      // print repo rows
+      if (typeof group.repos === 'string')
+        group.repos = [group.repos];
+      group.repos && group.repos.forEach(function(repo, index) {
+        var row = '\n    <tr>'
+
+        // add repo link
+        row += f('<td><a href="//github.com/%s">%s</a>',
+          repo, repo.split('/').pop());
+
+        if (index === 0) {
+          // add owner
+          row += f('\n        <td rowspan="%s" align="center">',
+            group.repos.length);
+          if (typeof group.owner === 'string')
+            group.owner = [group.owner];
+          group.owner && group.owner.forEach(function(owner, index) {
+            if (index !== 0)
+              row += '<br>'
+            row += owner;
+          });
+
+          // add backup
+          row += f('\n        <td rowspan="%s" align="center">',
+            group.repos.length);
+          if (typeof group.backup === 'string')
+            group.backup = [group.backup];
+          group.backup && group.backup.forEach(function(backup, index) {
+            if (index !== 0)
+              row += '<br>'
+            row += backup;
+          });
+        }
+
+        fs.appendFileSync(filePath, row);
+      });
+
+      // print table footer
+      fs.appendFileSync(filePath, '\n</table>');
+    }
+  });
+});
